c3633b88dddfcd97f51784dda62f6dddb05e7bd1
Rob Wells <rob@robw.tf>
2022-03-06 15:23:58 +0000

    make common numbers variables to cut down on size

--- build/make-tree.js
+++ build/make-tree.js
@@ -5,10 +5,21 @@ const reduce = require("stream-reduce");
 
 const writeTreeStream = fs.createWriteStream(path.resolve(__dirname, "../zip-tree.js"));
 
+const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_$";
+const dictionarysize = dictionary.length;
+
+const indexToVar = (index) => {
+    if (index < dictionarysize) {
+        return dictionary[index];
+    } else {
+        return indexToVar(Math.floor(index / dictionarysize) - 1) + indexToVar(index % dictionarysize);
+    }
+};
+
 process.stdin
     .pipe(es.split())
     .pipe(es.map((line, cb) => {
-        if(line === "") {
+        if (line === "") {
             return cb(null, "");
         }
         cb(null, line.substr(49))
@@ -22,14 +33,14 @@ process.stdin
             for (let i = 0; i < len - 1; i++) {
                 const digit = +zip[i];
 
-                if(i + 2 === len) {
-                    if(!treeLevel[digit]) {
+                if (i + 2 === len) {
+                    if (!treeLevel[digit]) {
                         treeLevel[digit] = 0;
                     }
-                    const finalDigit = +zip[i+1];
+                    const finalDigit = +zip[i + 1];
                     treeLevel[digit] |= (1 << (finalDigit));
                 } else {
-                    if(!treeLevel[digit]) {
+                    if (!treeLevel[digit]) {
                         treeLevel[digit] = [];
                     }
                     treeLevel = treeLevel[digit];
@@ -44,7 +55,77 @@ process.stdin
         const jsonString = JSON.stringify(treeData);
         const jsString = jsonString
             .replace(/null/g, '') // js can parse something like `[,,,,0]` just fine
-            .replace(/1023/g, '-1'); // -1 is like 0xFFFFFFFF in bitwise maths, so -1 = 1023, we can save 2 bytes each time
-        cb(null, `module.exports = ${jsString}`);
+            .replace(/1023/g, '-1') // -1 is like 0xFFFFFFFF in bitwise maths, so -1 = 1023, we can save 2 bytes each time
+            .replace(/1000/g, '1e3');
+
+        console.log("Pre number optimize size", `module.exports=${jsString}`.length);
+
+        const numberRegex = /(-?[0-9]+|1e3)/gi;
+
+        const matches = jsString.matchAll(numberRegex);
+
+        const numberCount = new Map();
+
+        for (const match of matches) {
+            const number = match[0];
+            if (numberCount.has(number)) {
+                numberCount.set(number, numberCount.get(number) + 1);
+            } else {
+                numberCount.set(number, 1);
+            }
+        }
+        const numbersToOptimize = [...numberCount.entries()]
+            .map(([number, count]) => ({
+                number,
+                count,
+                var1saved: (count * (number.length - 1)) - (3 + number.length),
+                var2saved: (count * (number.length - 2)) - (4 + number.length)
+            }))
+            .sort((a, b) => b.var1saved - a.var1saved);
+
+        const var1SizeNumbers = numbersToOptimize.slice(0, 2 * 26)
+            .map(({ number, count, var1saved }) => ({ number, count, saved: var1saved }));
+
+        const var2SizeNumbers = numbersToOptimize
+            .slice(2 * 26)
+            .sort((a, b) => b.var2saved - a.var2saved)
+            .map(({ number, count, var2saved }) => ({ number, count, saved: var2saved }));
+
+        const orderedNumbers = [
+            ...var1SizeNumbers,
+            ...var2SizeNumbers
+        ]
+            .filter(a => a.saved > 0)
+            .map((a, index) => ({ ...a, varName: indexToVar(index) }))
+
+        const vars = orderedNumbers.map((a) => a.varName + "=" + a.number)
+            .join(",")
+
+        const varsMap = orderedNumbers.reduce((obj, a) => {
+            obj[a.number] = a.varName;
+            return obj;
+        }, {});
+
+        const optimizedJsString = jsString.replace(numberRegex, (substring) => {
+            if (!varsMap[substring]) {
+                return substring;
+            }
+            return varsMap[substring];
+        });
+
+
+
+        // console.log(varsMap);
+
+        // orderedNumbers.forEach(a => {
+        //     const num = a.number[0] === "-" ? "\\" + a.number : a.number;
+        //     const regex = new RegExp(`(?:\\[${num}\\,|\\,${num}\\]|\\,${num}\\,|\\[${num}\\])`, 'g');
+        //     optimizedJsString = optimizedJsString.replace(regex, (substring) => {
+        //         // a.number === "-1" && console.log(a, substring.replace(a.number, a.varName));
+        //         return substring.replace(a.number, a.varName);
+        //     });
+        // });
+        console.log("Post number optimize size", `const ${vars};\nmodule.exports=${optimizedJsString}`.length, 22680);
+        cb(null, `const ${vars};\nmodule.exports=${optimizedJsString}`);
     }))
     .pipe(writeTreeStream)
